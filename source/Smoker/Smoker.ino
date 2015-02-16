9ca6dd9732faf5d0f41d6810857bea4b81e06d8a
Jason Robey <jason.robey@gmail.com>
2015-02-16 16:02:26 -0500

    Can now adjust target temp

--- source/Smoker/Smoker.ino
+++ source/Smoker/Smoker.ino
@@ -4,10 +4,28 @@
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
-int tempPin = 3; 
+int tempPin = 3;
 int lightPin = 2;
-int targetTemp = 24; // C
-float celsius; // Float to hold sensor value
+int upPin = 4;
+int downPin = 5;
+
+int upState = HIGH;      // the current state of the output pin
+int upReading;           // the current reading from the input pin
+int upPrevious = LOW;    // the previous reading from the input pin
+
+int downState = HIGH;      // the current state of the output pin
+int downReading;           // the current reading from the input pin
+int downPrevious = LOW;    // the previous reading from the input pin
+
+long upTime = 0;         // the last time the output pin was toggled
+long downTime = 0;         // the last time the output pin was toggled
+long debounce = 50;  // the debounce time, increase if the output flickers
+
+float targetTemp = 200.00; // F taget temp
+float threshold = 3.00;
+float increment = 5.00;
+float highThreshold;
+float lowThreshold;
 
 // Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 3
@@ -27,9 +45,14 @@ void setup()
 
 	// Set light/heat to output
 	pinMode(lightPin, OUTPUT);
+	pinMode(upPin, INPUT_PULLUP);
+	pinMode(downPin, INPUT_PULLUP);
 
-	// Turn light/Heatsource on to start
-	digitalWrite(lightPin, HIGH);
+	// Turn on Heatsource on to start
+	digitalWrite(lightPin, LOW);
+
+	// Set thresholds
+	setThresholds();
 
 	displayTargetMessage();
 
@@ -40,81 +63,108 @@ void setup()
 
 void loop()
 {
+	readButton(upPin, "UP");
+
+	readButton(downPin, "DOWN");
+
 	float reading = readTempSensor();
 
 	reportTemp(reading);
 
-	toggleLight(reading);
-
-	delay(5000);
-
+	toggleHeat(reading);
 }
 
-void toggleLight(float temp)
+void readButton(int buttonPin, String button)
 {
-	// If temp is above 24c Turn light off
-	if (temp >= targetTemp)
+	int reading = digitalRead(buttonPin);
+
+	// if the input just went from LOW and HIGH and we've waited long enough
+	// to ignore any noise on the circuit, toggle the output pin and remember
+	// the time
+	if (button == "UP")
 	{
-		digitalWrite(lightPin, LOW);
+		if (reading == LOW && upPrevious == HIGH && millis() - upTime > debounce) {
+			if (upState == LOW)
+				upState = HIGH;
+			else
+				upState = LOW;
+
+			upTime = millis();
+			Serial.println("Up button pushed");
+			targetTemp += increment;
+			setThresholds();
+			displayTargetMessage();
+		}
+
+		upPrevious = reading;
 	}
-	else
+	
+	if (button == "DOWN")
 	{
+		if (reading == LOW && downPrevious == HIGH && millis() - downTime > debounce) {
+			if (downState == LOW)
+				downState = HIGH;
+			else
+				downState = LOW;
+
+			downTime = millis();
+			Serial.println("Down button pushed");
+			targetTemp -= increment;
+			setThresholds();
+			displayTargetMessage();
+		}
+
+		downPrevious = reading;
+	}
+}
+
+
+void setThresholds()
+{
+	highThreshold = targetTemp + threshold;
+	lowThreshold = targetTemp - threshold;
+}
+
+void toggleHeat(float temp)
+{
+	if (temp >= highThreshold)
+	{
+		// turn off
 		digitalWrite(lightPin, HIGH);
+	}
+	else if (temp <= lowThreshold)
+	{
+		// turn on
+		digitalWrite(lightPin, LOW);
 	}
 }
 
 void reportTemp(float temp)
 {
 	Serial.print(temp);
-	Serial.println(" C");
-
-
-
-	Serial.print(convertToF(temp));
 	Serial.println(" F");
 
 	lcd.setCursor(0, 1);
 	String message = "Current: ";
-	message += convertToF(temp);
+	message += temp;
 	message += " F";
 	lcd.print(message);
 }
 
 float readTempSensor()
 {
-	// Using the factor is more effcient but not as clear what is going on
-	// celsius = analogRead(tempPin) * 0.48875855;
-
-	celsius = 0;
-
-	// I am not sure if we need this since we are using digital...
-	//// get 8 samples then report the average
-	//for (int i = 0; i <= 7; i++)
-	//{
-	//	tempSensors.requestTemperatures();
-	//	celsius += tempSensors.getTempCByIndex(0); //TODO: This may be slow, look for faster way
-	//}
-
-	//// Get the avg
-	//celsius = celsius / 8;
-
+	float temp = 0;
 	tempSensors.requestTemperatures();
+	temp = tempSensors.getTempFByIndex(0);
 
-	celsius = tempSensors.getTempCByIndex(0);
-
-	return celsius;
-}
-
-float convertToF(float celsius)
-{
-	return celsius * 1.8000 + 32.00;
+	return temp;
 }
 
 void displayTargetMessage()
 {
 	lcd.setCursor(0, 0);
 	String message = "Target: ";
-	message += convertToF(targetTemp);
+	message += targetTemp;
 	message += " F";
 	lcd.print(message);
 }
